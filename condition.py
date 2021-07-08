1db7593d148fab07153a63bc25275fb093b33d77
詹政勳 <nintendof1@gmail.com>
2021-07-08 18:34:35 +0800

    if else

--- condition.py
+++ condition.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 # 判斷式
-if True:
-    print("True 執行")
+# 三種判斷 if elif else
+# x=input("請輸入數字: ") # 取得字串形式的使用者輸入
+# x=int(x) # 將字串型態轉換成數字型態
+
+# if x>200:
+#     print("大於 200")
+# elif x>100:
+#     print("大於 100, 小於等於 200")
+# else:
+#     print("小於等於 100")
+
+# 四則運算
+n1=int(input("請輸入數字一: "))
+n2=int(input("請輸入數字二: "))
+op=input("請輸入運算: +, -, *, /: ")
+# 輸入運算時需加上雙引號，否則會報錯
+if op=="+":
+    print(n1+n2)
+elif op=="-":
+    print(n1-n2)
+elif op=="*":
+    print(n1*n2)
+elif op=="/":
+    print(n1/n2)
 else:
-    print("False 執行")
+    print("不支援的運算")
