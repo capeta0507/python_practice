d67941ea82d1188744936d48e6f861b48acb45cc
詹政勳 <nintendof1@gmail.com>
2021-07-08 17:38:52 +0800

    First commit

---
+++ condition.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# 判斷式
+if True:
+    print("True 執行")
+else:
+    print("False 執行")
---
+++ number-string.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# 數字運算
+# x=3+6
+# print(x)
+
+# x=3-6
+# print(x)
+
+# x=3*6
+# print(x)
+
+# # // 整除，/ 除以
+# x=3/6
+# print(x)
+
+# x=3//6
+# print(x)
+
+# x=3/7
+# print(x)
+
+# x=3//7
+# print(x)
+
+# x=7/6
+# print(x)
+
+# x=7//6
+# print(x)
+
+# # **次方
+# x=2**3
+# print(x)
+
+# # 開根號
+# x=2**0.5
+# print(x)
+
+# # 取餘數
+# x=7%3
+# print(x)
+
+# x=2+3
+# print(x)
+# x+=1 # x=x+1 # 將變數中數字 +1
+# print(x)
+# x-=1 # x=x-1 # 將變數中數字 -1
+# print(x)
+# 字串運算
+s="Hello"
+print(s)
+s="Hell\"o"
+print(s)
+# 字串組合
+s="Hello " "World"
+print(s)
+# 單換行
+s="Hello\nWorld"
+print(s)
+# 多行
+s="""Hello
+
+
+
+World"""
+print(s)
+# 字串會對內部字元編號（索引），從0開始算起
+s="Hello"
+print(s[1:4])
