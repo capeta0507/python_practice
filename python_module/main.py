b64bc9bdbc26807d71444646bf607414a1d8e838
詹政勳 <nintendof1@gmail.com>
2021-07-12 12:24:37 +0800

    init2

--- python_module/main.py
+++ python_module/main.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
-import sys
+# import sys
 
-sys.path.append("geometry") # 在模組的搜尋路徑列表中【新增路徑】
+# sys.path.append("geometry") # 在模組的搜尋路徑列表中【新增路徑】
 # 主程式
 
-# 暫無功用
-# import geometry.point
-# result=geometry.point.distance(3,4)
-
-import point
-result=point.distance(3,4)
+import geometry.point
+result=geometry.point.distance(3,4)
 print("距離 " + str(result))
-import line
-result=line.slope(1,1,3,4)
+
+import geometry.line
+result=geometry.line.slope(1,1,3,4)
 print("斜率 " + str(result))
+
+# import point
+# result=point.distance(3,4)
+# print("距離 " + str(result))
+# import line
+# result=line.slope(1,1,3,4)
+# print("斜率 " + str(result))
